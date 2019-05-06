0a8aa07571f60b96be6ad5b9156fd6ccfc89ed5f
frontalnh <frontalnh@gmail.com>
2019-05-06 22:35:49 +0900

    update

---
+++ get.js
@@ -0,0 +1,26 @@
+const AWS = require("aws-sdk");
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+module.exports.run = async (event)=>{
+  const params = {
+    TableName: "todos",
+    Key:{
+      id: event.pathParameters.id
+    }
+  }
+
+  const result = await dynamoDB.get(params).promise()
+
+  if(result.Item){
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Item)
+    };
+  }else{
+    return {
+      statusCode: 404,
+      body: JSON.stingify({message:"Couldn't find the todo item."})
+    };
+  }
+}
---
+++ handler.js
@@ -0,0 +1,4 @@
+module.exports.run = (event,context,callback)=>{
+  console.log("debugging...");
+  callback(null, "Hello world");
+}
